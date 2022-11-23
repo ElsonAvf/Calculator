6c824a1ef5cb03d14a381ea8b464cb55d8fe958c
ElsonAvf <twdgotcocminefds@gmail.com>
2022-11-23 15:17:04 -0300

    Add four basic math operations functions

---
+++ javascript/javascript.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  (b == 0) ? return 'Não pode dividir por zero' : return a / b
+}
