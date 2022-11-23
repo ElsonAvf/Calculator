c90017889148efe3308859de8dabe1ee0f3b82d2
ElsonAvf <twdgotcocminefds@gmail.com>
2022-11-23 15:46:19 -0300

    Create operate() function that gives a result based on the operator

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -11,5 +11,20 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  (b == 0) ? return 'Não pode dividir por zero' : return a / b
+ return (b == 0) ? 'Não pode dividir por zero' :  a / b
 }
+
+function operate(num1, operator, num2) {
+  switch (operator) {
+    case '+':
+      return add(num1, num2)
+    case '-':
+      return subtract(num1, num2)
+    case '*':
+      return multiply(num1, num2)
+    case '/':
+      return divide(num1, num2)
+  }
+}
+
+console.log(operate(4, '/', 2));
