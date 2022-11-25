cb893ea7abd99cee0fb738dedcc573639ab6285e
ElsonAvf <twdgotcocminefds@gmail.com>
2022-11-25 17:51:01 -0300

    Create a display system and make the calculator work

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -11,7 +11,7 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
- return (b == 0) ? 'Não pode dividir por zero' :  a / b
+ return (b == 0) ? alert('Não pode dividir por zero') :  a / b
 }
 
 function operate(num1, operator, num2) {
@@ -20,24 +20,96 @@ function operate(num1, operator, num2) {
       return add(num1, num2)
     case '-':
       return subtract(num1, num2)
-    case '*':
+    case 'x':
       return multiply(num1, num2)
-    case '/':
+    case '÷':
       return divide(num1, num2)
   }
 }
 
+// Variáveis para operações
+let numero1 = '', numero2 = '', operador = '', result;
+
 // Display inputs
-const screen = document.querySelector('#screen')
+const screen = document.querySelector('#screen');
 const buttons = document.querySelector('#main');
-buttons.addEventListener('click', (e) => {
-  if (e.target.textContent == 'C') {
-    screen.textContent = '';
-  } else if (e.target.classList == 'operator') {
-    screen.textContent = e.target.textContent;
-  } else if (screen.textContent.length < 9) {
-    let text = screen.textContent;
-    screen.textContent = text + e.target.textContent
+const numbers = document.querySelector('#numbers');
+
+function clean() {
+  screen.textContent = '',
+  numero1 = '';
+  operador = '';
+  numero2 = '';
+}
+
+function displayNumber(targetText) {
+     if (targetText == '.') {
+       testDotInput(targetText);
+     } else if (operador && numero1) {
+          if (!numero2) {
+            numero2 += targetText;
+            screen.textContent = numero2;
+          } else if (screen.textContent.length < 9) {
+               numero2 += targetText
+               screen.textContent = numero2;
+          }
+     }else {
+         if (screen.textContent.length < 9) {
+           numero1 += targetText;
+           screen.textContent = numero1;
+         }
+     }
+}
+
+
+function makeCalc() {
+  result = operate(parseFloat(numero1), operador, parseFloat(numero2))
+  screen.textContent = Math.round(parseFloat(result)*100)/100;
+  numero1 = result;
+  numero2 = '';
+  operador = '';
+}
+
+function setOperador(targetText) {
+  if (numero2) {
+    result = operate(parseFloat(numero1), operador, parseFloat(numero2))
+    screen.textContent = result;
+    numero1 = result;
+    operador = targetText;
+    numero2 = '';
+  } else {
+    operador = targetText;
   }
+}
+
+function testDotInput(targetText) {
+  if (numero2) {
+    if (!screen.textContent.match(/[.]/g)) {
+      numero2 += targetText;
+      screen.textContent = numero2;
+    }
+  } else {
+    if (!screen.textContent.match(/[.]/g)) {
+      numero1 += targetText;
+      screen.textContent = numero1;
+    }
+  }
+}
+
+buttons.addEventListener('click', (e) => {
+  // Para deixar mais legível
+  const targetText = e.target.textContent;
   
+  // Display Info
+  if (targetText == 'C') {
+    clean();
+  } else if (e.target.classList == 'operator' && targetText != '=') {
+    setOperador(targetText);
+  } else if (numbers.contains(e.target)) {
+    displayNumber(targetText);
+    }
+  
+  if (targetText == '=' && numero2 && numero1 && operador) {
+    makeCalc();
+  }
 });
